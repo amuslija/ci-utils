2c42288f25270baa80ca94d6478d416e38054566
Adnan Muslija <muslija.adnan@gmail.com>
2019-12-17 11:36:34 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const core = require('@actions/core');
+
+const logger = require('./utils/logger');
+const reportVulnerabilities = require('./lib/reportVulnerabilities');
+
+const main = async () => {
+  const command = core.getInput('command');
+
+  try {
+    switch (command) {
+      case 'report-vulnerabilities':
+        return reportVulnerabilities();
+      default:
+        logger.log('No command specified');
+        return process.exit(0);
+    }
+  } catch (e) {
+    logger.error(`Error during execution of ${command}: ${e.message}`, e);
+    return process.exit(1);
+  }
+};
+
+main();
